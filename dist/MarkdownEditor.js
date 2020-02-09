8c63ae2fdd8b18124bdf65b9343ea3dbfdea62e5
bbsmithy <bsmith@cartrawler.com>
2020-02-09 17:00:08 +0000

    Lastest lib

---
+++ dist/MarkdownEditor.js
@@ -0,0 +1,87 @@
+import _classCallCheck from "@babel/runtime/helpers/esm/classCallCheck";
+import _createClass from "@babel/runtime/helpers/esm/createClass";
+import _possibleConstructorReturn from "@babel/runtime/helpers/esm/possibleConstructorReturn";
+import _getPrototypeOf from "@babel/runtime/helpers/esm/getPrototypeOf";
+import _inherits from "@babel/runtime/helpers/esm/inherits";
+import * as React from "react";
+import "./MarkdownEditor.css";
+
+var MarkdownIt = require("markdown-it");
+
+var md = new MarkdownIt({
+  html: true // Enable HTML tags in source
+
+});
+
+var MdEditor =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(MdEditor, _React$Component);
+
+  function MdEditor(props) {
+    var _this;
+
+    _classCallCheck(this, MdEditor);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(MdEditor).call(this, props));
+
+    _this.createHTML = function (markdown) {
+      return md.render(markdown);
+    };
+
+    _this.onChangeMarkdown = function (evt) {
+      var html = _this.createHTML(evt.currentTarget.value);
+
+      _this.setState({
+        html: html
+      });
+    };
+
+    _this.onScrollMDContainer = function (evt) {
+      _this.htmlContainerRef.current.scrollTop = _this.mdContainerRef.current.scrollTop;
+      scrollHeightMD = _this.mdContainerRef.current.scrollTop;
+    };
+
+    _this.onScrollHTMLContainer = function (evt) {
+      _this.mdContainerRef.current.scrollTop = _this.htmlContainerRef.current.scrollTop;
+    };
+
+    _this.mdContainerRef = React.createRef();
+    _this.htmlContainerRef = React.createRef();
+    _this.state = {
+      html: ""
+    };
+    return _this;
+  }
+
+  _createClass(MdEditor, [{
+    key: "render",
+    value: function render() {
+      return React.createElement("div", {
+        className: "mainContainer",
+        style: {
+          height: this.props.height
+        }
+      }, React.createElement("div", {
+        className: "markdownContainer"
+      }, React.createElement("textarea", {
+        className: "markdownEditor",
+        onScroll: this.onScrollMDContainer,
+        ref: this.mdContainerRef,
+        onChange: this.onChangeMarkdown
+      })), React.createElement("div", {
+        className: "htmlContainer",
+        onScroll: this.onScrollHTMLContainer,
+        ref: this.htmlContainerRef
+      }, this.state.html && React.createElement("div", {
+        dangerouslySetInnerHTML: {
+          __html: this.state.html
+        }
+      })));
+    }
+  }]);
+
+  return MdEditor;
+}(React.Component);
+
+export { MdEditor as default };
---
+++ src/lib/MarkdownEditor.js
@@ -0,0 +1,65 @@
+import * as React from "react";
+import "./MarkdownEditor.css";
+
+const MarkdownIt = require("markdown-it");
+
+const md = new MarkdownIt({
+  html: true // Enable HTML tags in source
+});
+
+export default class MdEditor extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.mdContainerRef = React.createRef();
+    this.htmlContainerRef = React.createRef();
+
+    this.state = {
+      html: ""
+    };
+  }
+
+  createHTML = markdown => {
+    return md.render(markdown);
+  };
+
+  onChangeMarkdown = evt => {
+    const html = this.createHTML(evt.currentTarget.value);
+    this.setState({
+      html
+    });
+  };
+
+  onScrollMDContainer = evt => {
+    this.htmlContainerRef.current.scrollTop = this.mdContainerRef.current.scrollTop;
+    scrollHeightMD = this.mdContainerRef.current.scrollTop;
+  };
+
+  onScrollHTMLContainer = evt => {
+    this.mdContainerRef.current.scrollTop = this.htmlContainerRef.current.scrollTop;
+  };
+
+  render() {
+    return (
+      <div className="mainContainer" style={{ height: this.props.height }}>
+        <div className="markdownContainer">
+          <textarea
+            className="markdownEditor"
+            onScroll={this.onScrollMDContainer}
+            ref={this.mdContainerRef}
+            onChange={this.onChangeMarkdown}
+          ></textarea>
+        </div>
+        <div
+          className="htmlContainer"
+          onScroll={this.onScrollHTMLContainer}
+          ref={this.htmlContainerRef}
+        >
+          {this.state.html && (
+            <div dangerouslySetInnerHTML={{ __html: this.state.html }}></div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
